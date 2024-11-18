3150d3cfb5422ed626a321f44c69ec789356b93a
ykikuchi <y.kikuchi5101@gmail.com>
2024-11-18 15:55:10 +0900

    feat: signinのメソッドを実装

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { CredentialsDto } from './dto/credentials.dto';
+import { JwtPayload } from 'src/types/jwtPayload';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     const { name, email, password, status } = createUserDto;
@@ -16,5 +22,25 @@ export class AuthService {
     return await this.prismaService.user.create({
       data: { name, email, password: hashedPassword, status },
     });
+  }
+
+  async signIn(credentialsDto: CredentialsDto): Promise<{ token: string }> {
+    const { email, password } = credentialsDto;
+    const user = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload: JwtPayload = {
+        sub: user.id,
+        username: user.name,
+        status: user.status,
+      };
+
+      const token = this.jwtService.sign(payload);
+      return { token };
+    }
+
+    throw new UnauthorizedException();
   }
 }
