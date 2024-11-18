0ef0c0b657abb17dc998caddb96c4653181191a5
ykikuchi <y.kikuchi5101@gmail.com>
2024-11-18 11:04:33 +0900

    feat:itemsのCRUDを実装

---
+++ src/items/items.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { Item } from './items.model';
+
+@Injectable()
+export class ItemsService {
+  private items: Item[] = [];
+
+  findAll(): Item[] {
+    return this.items;
+  }
+
+  findById(id: string): Item {
+    return this.items.find((item) => item.id === id);
+  }
+
+  create(item: Item): Item {
+    this.items.push(item);
+    return item;
+  }
+
+  updateStatus(id: string): Item {
+    const item = this.findById(id);
+    item.status = 'SOLD_OUT';
+    return item;
+  }
+
+  delete(id: string) {
+    this.items = this.items.filter((item) => item.id !== id);
+  }
+}
