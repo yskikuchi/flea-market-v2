a6bff3fd32828f5569f55c6d3e1913a5d35639d1
ykikuchi <y.kikuchi5101@gmail.com>
2024-11-18 11:49:01 +0900

    feat: validationを追加

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,5 +1,24 @@
+import {
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  Min,
+} from 'class-validator';
+
 export class CreateItemDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(40)
   name: string;
+
+  @IsInt()
+  @Min(1)
   price: number;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(1000)
   description?: string;
 }
