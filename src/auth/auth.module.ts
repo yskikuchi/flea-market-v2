a6da5933e1b78b9cb3195fbafa541fae37ee9886
ykikuchi <y.kikuchi5101@gmail.com>
2024-11-18 15:43:42 +0900

    feat: 認証関連のライブラリをインストール

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
