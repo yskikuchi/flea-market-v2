a11a48348dc55912b1dfff28a4167eea0ad28146
ykikuchi <y.kikuchi5101@gmail.com>
2024-11-26 17:09:59 +0900

    docs: openAPI出力のための設定を追加

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,16 @@ async function bootstrap() {
   //   origin: ['http://example.com:1234'],
   //   methods: ['GET', 'POST'],
   // });
+
+  const config = new DocumentBuilder()
+    .setTitle('FLEA MARKET API')
+    .setDescription('FLEA MARKET API description')
+    .setVersion('1.0')
+    .addTag('flea-market')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
