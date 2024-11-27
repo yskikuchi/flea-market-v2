bbed9057e9e7fe979398ba721c16735f4adf46f2
ykikuchi <y.kikuchi5101@gmail.com>
2024-11-27 11:43:44 +0900

    feat: middlewareの動作確認

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
+import { ItemsController } from './items/items.controller';
 
 @Module({
   imports: [ItemsModule, PrismaModule, AuthModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(ItemsController);
+  }
+}
